4e988dcc6b62d8e8f26a7da277a135d07376aedd
Mustafa <30720101+akbasmustafa@users.noreply.github.com>
2021-07-05 13:36:39 +0100

    test enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { shallow, configure } from "enzyme";
+
+import Adaptor from "@wojtekmaj/enzyme-adapter-react-17";
+
+configure({ adapter: new Adaptor() });
+
+describe("Test App render", () => {
+  test("render title", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("h1").text()).toContain("School App");
+  });
+
+  test("render Router", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("Route").length).toBe(2);
+  });
 });
